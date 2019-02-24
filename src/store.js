5f4a305dc124bca6f8c06284e2ddd653845e8a67
Jeferson S. Brito <jefersonsilva4k@hotmail.com>
2019-02-24 19:15:55 -0300

    Fetch Events using Vuex

--- src/store.js
+++ src/store.js
@@ -51,15 +51,20 @@ export default new Vuex.Store({
           console.error(`There was an error: ${error.response}`)
         })
     },
-    fetchEvent({ commit }, id) {
-      EventService.getEvent(id)
-        .then(response => {
-          commit('SET_EVENT', response.data)
-        })
-        .catch(error => {
-          // eslint-disable-next-line no-console
-          console.error(`There was an error: ${error.response}`)
-        })
+    fetchEvent({ commit, getters }, id) {
+      const event = getters.getEventById(id)
+      if (event) {
+        commit('SET_EVENT', event)
+      } else {
+        EventService.getEvent(id)
+          .then(response => {
+            commit('SET_EVENT', response.data)
+          })
+          .catch(error => {
+            // eslint-disable-next-line no-console
+            console.error(`There was an error: ${error.response}`)
+          })
+      }
     }
   },
   getters: {
