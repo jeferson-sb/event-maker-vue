8d9ff21ca667a0322b9cb91b084f8169f1aada40
Jeferson S. Brito <jefersonsilva4k@hotmail.com>
2019-02-24 21:08:00 -0300

    Add notifications vuex module

---
+++ src/components/NotificationBar.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="notification-bar" :class="notificationTypeClass">
+    <p>{{ notification.message }}</p>
+  </div>
+</template>
+
+<script>
+import { mapActions } from 'vuex'
+
+export default {
+  props: {
+    notification: {
+      type: Object,
+      required: true
+    }
+  },
+  data() {
+    return {
+      timeout: null
+    }
+  },
+  mounted() {
+    this.timeout = setTimeout(() => this.remove(this.notification), 3000)
+  },
+  beforeDestroy() {
+    clearTimeout(this.timeout)
+  },
+  computed: {
+    notificationTypeClass() {
+      return `-text-${this.notification.type}`
+    }
+  },
+  methods: mapActions('notification', ['remove'])
+}
+</script>
+
+<style scoped>
+.notification-bar {
+  margin: 1em 0 1em;
+  box-shadow: 1px 1px 1px rgba(0,0,0,.1);
+  border: 2px solid #303030;
+  border-radius: 4px;
+  padding: .3rem 1rem;
+  transition: all 1s ease-in-out;
+}
+</style>
