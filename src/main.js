ff7188d6551955cc746817d0436c1889d2b7a875
Jeferson S. Brito <jefersonsilva4k@hotmail.com>
2020-03-15 21:16:39 -0300

    refactor: add reusable form components

--- src/main.js
+++ src/main.js
@@ -1,10 +1,18 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-import store from './store/store'
+import store from './store'
+
 import BaseIcon from '@/components/BaseIcon.vue'
+import BaseInput from '@/components/BaseInput.vue'
+import BaseSelect from '@/components/BaseSelect.vue'
+import BaseButton from '@/components/BaseButton.vue'
+import 'nprogress/nprogress.css'
 
 Vue.component('BaseIcon', BaseIcon)
+Vue.component('BaseInput', BaseInput)
+Vue.component('BaseSelect', BaseSelect)
+Vue.component('BaseButton', BaseButton)
 Vue.config.productionTip = false
 
 new Vue({
--- src/store/index.js
+++ src/store/index.js
@@ -21,6 +21,6 @@ export default new Vuex.Store({
       'education',
       'food',
       'community'
-    ],
+    ]
   }
 })
