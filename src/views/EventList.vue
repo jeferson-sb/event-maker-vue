4b88c05e3440407162437193edbe2c1195347897
Jeferson S. Brito <jefersonsilva4k@hotmail.com>
2020-03-15 21:19:13 -0300

    style: add nprogress bar

--- src/views/EventList.vue
+++ src/views/EventList.vue
@@ -1,42 +1,77 @@
 <template>
   <div>
     <h1>Events for {{ user.user.name }}</h1>
-    <EventCard v-for="event in event.events" :key="event.id" :event="event"/>
+    <transition-group name="slide-up" tag="div">
+      <EventCard v-for="event in event.events" :key="event.id" :event="event" />
+    </transition-group>
     <template v-if="page != 1">
-      <router-link :to="{ name: 'event-list', query: { page: page - 1 } }" rel="prev">Prev Page</router-link>
+      <router-link
+        :to="{ name: 'event-list', query: { page: page - 1 } }"
+        rel="prev"
+        >Prev Page</router-link
+      >
       <template v-if="hasNextPage">|</template>
     </template>
     <router-link
       v-if="hasNextPage"
       :to="{ name: 'event-list', query: { page: page + 1 } }"
       rel="next"
-    >Next Page</router-link>
+      >Next Page</router-link
+    >
   </div>
 </template>
 
 <script>
 import EventCard from '@/components/EventCard.vue'
 import { mapState } from 'vuex'
+import store from '@/store'
+
+function getPageEvents(routeTo, next) {
+  const currentPage = parseInt(routeTo.query.page) || 1
+  store
+    .dispatch('event/fetchEvents', {
+      page: currentPage
+    })
+    .then(() => {
+      routeTo.params.page = currentPage
+      next()
+    })
+}
 
 export default {
+  props: {
+    page: {
+      type: Number,
+      required: true
+    }
+  },
   components: {
     EventCard
   },
-  created() {
-    this.perPage = 3
-    this.$store.dispatch('event/fetchEvents', {
-      perPage: this.perPage,
-      page: this.page
-    })
+  beforeRouteEnter(routeTo, routeFrom, next) {
+    getPageEvents(routeTo, next)
+  },
+  beforeRouteUpdate(routeTo, routeFrom, next) {
+    getPageEvents(routeTo, next)
   },
   computed: {
-    page() {
-      return parseInt(this.$route.query.page) || 1
-    },
     hasNextPage() {
-      return this.event.eventsTotal > this.page * this.perPage
+      return this.event.eventsTotal > this.page * this.event.perPage
     },
-    ...mapState(['event', 'eventsTotal', 'user'])
+    ...mapState(['event', 'user'])
   }
 }
 </script>
+
+<style scoped>
+.slide-up-enter {
+  opacity: 0;
+  transform: translateY(10px);
+}
+.slide-up-enter-active {
+  transition: all 0.5s ease;
+}
+.slide-up-move {
+  transition: transform 0.5s ease-out;
+}
+</style>
