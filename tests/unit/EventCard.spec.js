6560c6ee61b1fc47cc51e8fb9ff5f21bbd7f91b0
Jeferson S. Brito <jefersonsilva4k@hotmail.com>
2022-01-23 18:47:31 -0300

    fix: update testing

--- tests/unit/EventCard.spec.js
+++ tests/unit/EventCard.spec.js
@@ -1,14 +1,13 @@
+import { render } from '@testing-library/vue'
 import EventCard from '@/components/EventCard'
-import { shallowMount } from '@vue/test-utils'
+
 import { events } from './dbTest.json'
 
-const mockData = { event: events[0] }
-
-describe('EventCard', () => {
+describe('<EventCard />', () => {
   test('it should mounts component with props', () => {
-    const wrapper = shallowMount(EventCard, {
-      propsData: mockData
-    })
-    expect(wrapper.vm.$props).toMatchObject(mockData)
+    const { getByText } = render(EventCard, { props: { eventData: events[0] } })
+    const title = getByText('Exercise Practice')
+
+    expect(title).toBeInTheDocument()
   })
 })
